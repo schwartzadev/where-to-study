731ec099688c5a5794b2cf7ff2a5ea8612c73adc
Andrew Schwartz <andrew@aschwartz.me>
2021-03-06 14:06:51 -0700

    feat: filter rooms based on bookings

--- study/study/utils.py
+++ study/study/utils.py
@@ -15,7 +15,8 @@ def get_building_density(day_of_week: str) -> dict:
         data = json.load(f)
     return data
 
-def get_room_availability_data() -> dict:
+
+def get_building_availability_data() -> dict:
     """Gets and parses availability data from ems.colorado.edu
 
     Returns:
@@ -51,7 +52,7 @@ def get_room_availability_data() -> dict:
 
     clean_buildings = []
     for building in raw_buildings_data:
-        rooms = [room['Description'] for room in building['Rooms']]
+        rooms = [{ "name": room['Description'], "id": room['Id']} for room in building['Rooms']]
         
         # Don't add the building if it has no rooms.
         if len(rooms) > 0:
@@ -69,11 +70,47 @@ def get_current_building_density() -> dict:
     return get_building_density(today_weekday)
 
 
+def filter_rooms_by_bookings(original_rooms: list) -> list:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/json; charset=utf-8',
+        'dea-CSRFToken': 'cbb94c0b-118b-471e-9a07-aba59b53a5b3',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://ems.colorado.edu',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://ems.colorado.edu/BrowseForSpace.aspx',
+        'TE': 'Trailers',
+    }
+
+    # todo make the date values dynamic
+    data = '{"filterData":{"filters":[{"filterName":"StartDate","value":"2021-03-6 12:00:00","displayValue":null,"filterType":3},{"filterName":"EndDate","value":"2021-03-7 12:00:00","filterType":3,"displayValue":""},{"filterName":"Locations","value":"-1","displayValue":"(all)","filterType":8},{"filterName":"TimeZone","value":"68","displayValue":"","filterType":2},{"filterName":"RoomTypes","value":"120","displayValue":"Study","filterType":7}]}}'
+
+    response = requests.post('https://ems.colorado.edu/ServerApi.aspx/GetBrowseLocationsBookings', headers=headers, data=data)
+
+    data = json.loads(response.content)
+
+    d = data['d']
+
+    bookings = json.loads(d)['Bookings']
+
+    # This filters out any room that has a booking in the current time period.
+    # This does *not* filter out rooms based on the time of the booking.
+
+    booked_rooms = [room['BookingInRoomId'] for room in bookings]
+
+    available_rooms = [room for room in original_rooms if room['id'] not in booked_rooms]
+
+    return available_rooms
+
+
 def get_available_rooms():
     """
     Combines density data and available room data.
     """
-    available_buildings =  get_room_availability_data()
+    available_buildings =  get_building_availability_data()
     current_density = get_current_building_density()
 
     density_dict = {}
@@ -95,9 +132,11 @@ def get_available_rooms():
     for building in available_buildings:
         for room in building['rooms']:
             rooms.append({
-                "label": room,
+                "label": room['name'],
+                "id": room['id'],
                 "building_code": building["code"],
-                "density": building['density']
+                "density": building['density'] # todo make this just the current density %
             })
 
-    return rooms
+    filtered_rooms = filter_rooms_by_bookings(rooms)
+    return filtered_rooms
