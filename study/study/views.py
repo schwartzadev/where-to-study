149adf931c9a5f9a2a4b75b3beefcb3e8b5c00b3
Andrew Schwartz <andrew@aschwartz.me>
2021-03-06 17:43:58 -0700

    fix(index): context

--- study/study/views.py
+++ study/study/views.py
@@ -8,8 +8,4 @@ class MapView(TemplateView):
 
 class IndexView(TemplateView):
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        room = choose_room.choose_random() # debugging
-        return context
+    extra_context={'room': choose_room.choose_random()}
